2e33e2982ebe6dfe0bf50d7d750ff8ae230c1fab
zachares <32395644+zachares@users.noreply.github.com>
2021-02-24 04:58:46 -0800

    Update software_requirements.sh
    changed installation requirements to reflect changes in deep_learning repository

--- software_requirements.sh
+++ software_requirements.sh
@@ -73,8 +73,9 @@ cd ..
 # pip install -e .
 # cd ..
 
-git clone -b https://github.com/zachares/supervised_learning.git
-cd supervised_learning
+git clone https://github.com/zachares/deep_learning.git
+git checkout 74101ca544c7ee8365657c6ba26c92d9ff54a21dcd
+cd deep_learning
 pip install -e .
 cd ..
 
